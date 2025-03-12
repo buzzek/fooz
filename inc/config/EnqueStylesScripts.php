923dbbf961b45bdab67d4c9d74918802f3f5b8f5
buzzek90 <buzzpoland@gmail.com>
2025-03-12 11:33:52 +0100

    REFINE : update comments for script and style enqueuing

--- inc/config/EnqueStylesScripts.php
+++ inc/config/EnqueStylesScripts.php
@@ -9,38 +9,46 @@ class EnqueStylesScripts
 {
 
     /**
-     * Constructor
+     * Adds actions to enqueue scripts and styles
      */
     public function __construct()
     {
+        // Hook to enqueue scripts
         add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
+        // Hook to enqueue styles
         add_action('wp_enqueue_scripts', [$this, 'enqueue_styles']);
     }
 
     /**
-     * Enques all scripts
+     * Enqueues all scripts
      */
     public function enqueue_scripts()
     {
+        // Enqueue the main JavaScript file with jQuery as a dependency
         wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/dist/scripts.js', ['jquery'], VER, true);
+
+        // Localize script to pass the AJAX URL to the script
         wp_localize_script('main-js', 'ajax_object', [
             'ajax_url' => admin_url('admin-ajax.php'),
         ]);
     }
 
     /**
-     * Enques all styles
+     * Enqueues all styles
      */
     public function enqueue_styles()
     {
+        // Enqueue the parent theme's stylesheet
         wp_enqueue_style('twentytwentyfive-parent-style', get_template_directory_uri() . '/style.css');
 
+        // Register the main stylesheet for the child theme
         wp_register_style(
             'main-css',
             get_stylesheet_directory_uri() . '/dist/style.css',
             ['twentytwentyfive-parent-style'],
             VER
         );
+        // Enqueue the main stylesheet for the child theme
         wp_enqueue_style('main-css');
     }
 }
